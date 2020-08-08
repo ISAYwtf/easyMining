01ccd24662f4bb1ca5749f39ecd098efa0b8142b
Iskander <iskander.aydynov@mail.ru>
2020-08-08 13:32:37 +0300

    bg, butt up in index.html

--- assets/js/body.js
+++ assets/js/body.js
@@ -1,8 +1,14 @@
-let header = document.querySelector('header');
+let header = document.querySelector('header'),
+    hoisting = document.querySelector('.hoisting');
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 40) {
         header.style.height = '120px';
+        if (window.scrollY > window.screen.height) {
+            hoisting.style.display = 'unset';
+        } else if (window.scrollY <= window.screen.height) {
+            hoisting.style.display = 'none';
+        }
     } else {
         header.style.height = '';
     }
