d8dc3e9f10de107d406c7f16722ac49fcbf1574c
Iskander <iskander.aydynov@mail.ru>
2021-03-26 23:51:55 +0300

    refactoring index.html and all linked css, media 1200px

--- assets/js/body.js
+++ assets/js/body.js
@@ -1,45 +1,45 @@
-let header = document.querySelector('header'),
-    hoisting = document.querySelector('.hoisting');
+const header = document.querySelector("header"),
+    hoisting = document.querySelector(".hoisting");
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
     if (window.scrollY > 40 && window.innerWidth > 770) {
-        header.style.height = '120px';
+        header.style.height = "90px";
 
         if (window.scrollY > window.innerHeight) {
-            hoisting.style.display = 'unset';
+            hoisting.style.display = "unset";
         } else if (window.scrollY <= window.innerHeight) {
-            hoisting.style.display = 'none';
+            hoisting.style.display = "none";
         }
     } else if (window.scrollY <= 40 && window.innerWidth > 770) {
-        header.style.height = '';
+        header.style.height = "";
     }
 });
 
-let burgerBtn = document.querySelector('.burger__btn'),
-    headerWrap = header.querySelector('.header--wrap');
+const burgerBtn = document.querySelector(".burger__btn"),
+    headerWrap = header.querySelector(".header--wrap");
 
-burgerBtn.addEventListener('click', () => {
+burgerBtn.addEventListener("click", () => {
     headerWrap.classList.toggle("paused");
 
     if (headerWrap.classList.contains("header--active")) {
         headerWrap.classList.remove("header--active");
         headerWrap.classList.add("reverse");
         header.style.height = "70px";
-        burgerBtn.children[0].children[0].removeAttribute('x');
-        burgerBtn.children[0].children[1].setAttribute('x', '5.66667');
+        burgerBtn.children[0].children[0].removeAttribute("x");
+        burgerBtn.children[0].children[1].setAttribute("x", "5.66667");
     } else if (headerWrap.classList.contains("reverse")) {
         headerWrap.classList.remove("reverse");
         headerWrap.classList.add("header--active");
         header.style.height = "271px";
-        headerWrap.style.display = 'flex';
-        burgerBtn.children[0].children[0].setAttribute('x', '17');
-        burgerBtn.children[0].children[1].setAttribute('x', '3');
+        headerWrap.style.display = "flex";
+        burgerBtn.children[0].children[0].setAttribute("x", "17");
+        burgerBtn.children[0].children[1].setAttribute("x", "3");
     } else {
         headerWrap.classList.add("header--active");
-        headerWrap.style.display = 'flex';
+        headerWrap.style.display = "flex";
         header.style.height = "271px";
-        burgerBtn.children[0].children[0].setAttribute('x', '17');
-        burgerBtn.children[0].children[1].setAttribute('x', '3');
+        burgerBtn.children[0].children[0].setAttribute("x", "17");
+        burgerBtn.children[0].children[1].setAttribute("x", "3");
     }
 
     setTimeout(() => {
@@ -49,36 +49,36 @@ burgerBtn.addEventListener('click', () => {
         } else if (headerWrap.classList.contains("reverse")) {
             headerWrap.style.height = "0";
             headerWrap.style.marginTop = "-130px";
-            headerWrap.style.display = 'none';
+            headerWrap.style.display = "none";
         }
 
         headerWrap.classList.toggle("paused");
     }, 400);
 });
 
-let simpleForm = document.querySelector('.form').querySelector('form'),
-    simpleFormBtn = simpleForm.querySelector('button');
+const simpleForm = document.querySelector(".form").querySelector("form"),
+    simpleFormBtn = simpleForm.querySelector("button");
 
-simpleForm.addEventListener('submit', (event) => {
+simpleForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
     let formData = {
-        email: simpleForm.elements.email.value
+        email: simpleForm.elements.email.value,
     };
 
     let request = new XMLHttpRequest();
 
-    request.addEventListener('load', () => {
+    request.addEventListener("load", () => {
         console.log(request.response);
-        if (request.response === '1') {
-            simpleFormBtn.classList.add('form-email__response');
-            simpleForm.elements.email.value = '';
-        } else  {
-            simpleFormBtn.classList.add('form-email__response-fail');
+        if (request.response === "1") {
+            simpleFormBtn.classList.add("form-email__response");
+            simpleForm.elements.email.value = "";
+        } else {
+            simpleFormBtn.classList.add("form-email__response-fail");
         }
     });
 
-    request.open('POST', 'assets/simple_mail.php', true);
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    request.open("POST", "assets/simple_mail.php", true);
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
     request.send(`email= ${encodeURIComponent(formData.email)}`);
 });
